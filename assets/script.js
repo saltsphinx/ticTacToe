0cd0f8c136a346f5ea13f483ff2adf0bc8ad5d69
tremar thomas <tremar.thomas19@gmail.com>
2022-10-16 12:33:28 -0500

    Complete basic round logic

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,80 @@
-const gameModule = (function (){
-  const playerFactory = function(name, char) {
-    let losses = 0;
-    let wins = 0;
-    return {name, char, losses, wins}
+const playerFactory = function(char) {
+  let losses = 0;
+  let wins = 0;
+  return {char, losses, wins}
+}
+
+const boardModule = (function() {
+  // the 9 array items represent 3 rows and 3 columns. Index of 0 is row and column 1. index of 2 is row 2, column 1
+  const boardArray = Array(9);
+
+  const markSquare = function(strChar, intIndex) {
+    if (intIndex <= 8 && boardArray[intIndex] == null) {
+      boardArray[intIndex] = strChar;
+      return boardArray;
+    } else {
+      console.log(`Index ${intIndex} not found or claimed already.`);
+    }
   }
 
-  const boardModule = (function() {
-    // the 9 array items represent 3 rows and 3 columns. Index of 0 is row and column 1. index of 2 is row 2, column 1
-    const boardArray = Array(9);
+  const checkBoard = function() {
+    let winner = checkHorizontal() || checkVertical() || checkDiagonal();
+    return winner;
+  }
 
-    const markSquare = function(strChar, intIndex) {
-      if (intIndex <= 8 && boardArray[intIndex] == null) {
-        boardArray[intIndex] = strChar;
-        return boardArray;
-      } else {
-        console.log(`Index ${intIndex} not found or claimed already.`);
-      }
-    }
-
-    const checkBoard = function() {
-      //m stands for multiple and represents the row, and increments by 3 for the horiziontal pattern
-      let m = 0;
+  const checkVertical = function() {
+    for (let i = 0; i <= 2; i++) {
       let patternArr = [];
-
-      while (m <= 6) {
-        // i stands for increment(1) and represents the column
-        for (let i = 0; i <= 2; i++) {
-          patternArr.push(boardArray[m + i]);
-        }
-
-        console.log(patternArr);
-        if (patternArr[0] && patternArr.every(x => x == patternArr[0])) console.log("win");
-        patternArr = [];
-        m += 3;
+      for (let j = 0; j <= 6; j += 3) {
+        patternArr.push(boardArray[i + j])
       }
-    }
 
-    return {boardArray, markSquare, checkBoard};
-  })();
+      if (patternArr[0] && patternArr.every(x => x == patternArr[0])) return patternArr[0];
+    }
+  }
+
+  const checkHorizontal = function() {
+    for (let i = 0; i <= 6; i += 3) {
+      let patternArr = [];
+      for (let j = 0; j <= 2; j++) {
+        patternArr.push(boardArray[i + j]);
+      }
+
+      if (patternArr[0] && patternArr.every(x => x == patternArr[0])) return patternArr[0];
+    }
+  }
+
+  const checkDiagonal = function() {
+    //Top left to bottom right
+    let patternArr = [];
+    for (let i = 0; i <= 8; i += 4) {
+      patternArr.push(boardArray[i]);
+    }
+    if (patternArr[0] && patternArr.every(x => x == patternArr[0])) return patternArr[0];
+    patternArr = [];
+
+    //Top right to bottom left
+    for (let i = 2; i <= 6; i += 2) {
+      patternArr.push(boardArray[i]);
+    }
+    if (patternArr[0] && patternArr.every(x => x == patternArr[0])) return patternArr[0];
+  }
+
+  const resetBoard = () => boardArray = [];
+
+  return {boardArray, markSquare, checkBoard};
+})();
+
+const gameModule = (function (){
+  const playerOne = playerFactory('X');
+  const playerTwo = playerFactory('O');
+  const board = boardModule;
 
   const initiateGame = function() {
 
   }
 
-  return {initiateGame, playerFactory, boardModule};
+  return {initiateGame, board};
 })();
 
 const game = gameModule;
